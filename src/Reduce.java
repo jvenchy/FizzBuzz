6f751cc4b247cbb65ae10412c3909fb8fa3b67fa
jvenchy <joshshergill@gmail.com>
2023-09-11 16:20:07 -0400

    new program for task, reduce.

---
+++ src/Reduce.java
@@ -0,0 +1,18 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int step = 0;
+
+        while (n != 0) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n -= 1;
+            }
+
+            step += 1;
+            System.out.println(step + " steps");
+        }
+
+    }
+}
